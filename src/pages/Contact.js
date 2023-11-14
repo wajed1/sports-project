9860ac191c9f028ff1cc9e588d45ef9e43a1ad35
Hadigh17 <hadighandour3@gmail.com>
2023-11-14 21:45:55 +0200

    Contact Us Done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import './Contact.css'
+
 function Contact() {
-    return(
-        <section className='hero'>
-            <h1>Contact</h1>
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    async function submit(e) {
+        e.preventDefault();
+        const templateParams = {
+            to_email: 'hadighandour3@gmail.com',
+            from_name: e.target.from_name.value,
+            from_email: e.target.from_email.value,
+            message: e.target.message.value,
+          };
+      
+          emailjs.send(
+            'service_ayyl51h',
+            'template_3x0e21d',
+            templateParams, 
+            'IptLCqLDRbLkd0Ij5'
+          )
+            .then((response) => {
+              console.log('Email sent:', response);
+              alert("Email sent sucessfully")
+              // Handle success or show a success message
+              setUsername('');
+              setEmail('');
+              setMessage('');
+            })
+            .catch((error) => {
+              console.error('Error sending email:', error);
+              alert("Error sending email.")
+              // Handle error or display an error message
+            });
+        }
+    return (
+        <section>
+        <div class='container1'>
+            <form onSubmit={submit}>
+                <h3>GET IN TOUCH</h3>
+                <input type='text' name='from_name' placeholder='User name' value={username} onChange={(e) => setUsername(e.target.value)} required></input>
+                <input type='text' name='from_email' placeholder='Email Adresse' value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+                <textarea name='message' rows='4' placeholder="How can we help you?" value={message} onChange={(e) => setMessage(e.target.value)}required></textarea>
+                <button type='submit'>Send Message</button>
+            </form>
+        </div>
+        <div class='footer'>
+                <li class='quick'>
+                    <h1>Quick Links</h1>
+                    <a href="/home">Home Page</a>
+                    <a href="/about">About Us</a>
+                    <a href="/">Log In</a>
+                </li>
+                <li class='contact'>
+                    <h1>Contact Members</h1>
+                    <ul>
+                        <li>Hadi Ghandour +96170711506</li>
+                        <li>Wajed basbous +96176045923</li>
+                        <li>Ahmad Dia +96170054234</li>
+                        <li>Jad Abdallah +96171333931</li>
+                    </ul>
+                </li>
+                <div class='follow'>
+                    <h1>Follow us on</h1>
+                    <ul>
+                    <a href=''>Instagram</a>
+                    <a href=''>Facebook</a>
+                    </ul>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,53 +1,79 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Login.css'
 import axios from "axios"
-import { Form, useNavigate} from "react-router-dom"
+import { Form, useNavigate } from "react-router-dom"
 
 
-function Login(){
+function Login() {
     const [username, setUsername] = useState('');
     const [pass, setPass] = useState('');
-    const history=useNavigate();
-    async function submit(e){
+    const history = useNavigate();
+    async function submit(e) {
         e.preventDefault();
 
-        try{
+        try {
 
-            await axios.post("http://localhost:8000/",{
-                username,pass
+            await axios.post("http://localhost:8000/", {
+                username, pass
             })
-            .then(res=>{
-                if(res.data==="exist"){
-                    history("/home",{state:{id:username}})
-                }
-                else if(res.data==="notexist"){
-                    alert("User did not sign up or the password is wrong")
-                }
-            })
-            .catch(e=>{
-                alert("wrong details")
-                console.log(e);
-            })
+                .then(res => {
+                    if (res.data === "exist") {
+                        history("/home", { state: { id: username } })
+                    }
+                    else if (res.data === "notexist") {
+                        alert("User did not sign up or the password is wrong")
+                    }
+                })
+                .catch(e => {
+                    alert("wrong details")
+                    console.log(e);
+                })
 
         }
-        catch(e){
+        catch (e) {
             console.log(e);
 
         }
 
     }
 
-    return(
-        <div className='cover'>
-            <h1 className='Login'>Login</h1>
-            <form action='POST'>
-            <input className='input-field ' value={username} type="text" placeholder='username' onChange={(e) => setUsername(e.target.value)}/>
-            <input className='input-field ' value={pass} type="password" placeholder='password' onChange={(e) => setPass(e.target.value)}/>
-            <button className='login-button' type='submit' onClick={submit}>Log In</button>
-            </form>
-                <a className='link' href='/'>Forgot password?</a>
-                <a className='link' href='/SignUp'>Sign Up instead</a>   
-        </div>
+    return (
+        <section>
+            <div className='cover'>
+                <h1 className='Login'>Login</h1>
+                <form action='POST'>
+                    <input className='input-field ' value={username} type="text" placeholder='username' onChange={(e) => setUsername(e.target.value)} />
+                    <input className='input-field ' value={pass} type="password" placeholder='password' onChange={(e) => setPass(e.target.value)} />
+                    <button className='login-button' type='submit' onClick={submit}>Log In</button>
+                </form>
+                <a className='link1' href='/'>Forgot password?</a>
+                <a className='link2' href='/SignUp'>Sign Up instead</a>
+            </div>
+            <div class='footer'>
+                <li class='quick'>
+                    <h1>Quick Links</h1>
+                    <a href="/home">Home Page</a>
+                    <a href="/about">About Us</a>
+                    <a href="/contact">Contact Us</a>
+                </li>
+                <li class='contact'>
+                    <h1>Contact Members</h1>
+                    <ul>
+                        <li>Hadi Ghandour +96170711506</li>
+                        <li>Wajed basbous +96176045923</li>
+                        <li>Ahmad Dia +96170054234</li>
+                        <li>Jad Abdallah +96171333931</li>
+                    </ul>
+                </li>
+                <div class='follow'>
+                    <h1>Follow us on</h1>
+                    <ul>
+                        <a href=''>Instagram</a>
+                        <a href=''>Facebook</a>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 }
 
