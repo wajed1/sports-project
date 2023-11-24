0c9c875d5df7ed4ff3e1f0c7cd6f0767866b8917
wajed1 <wajed.basbous@net.usj.edu.lb>
2023-11-24 09:59:05 +0200

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,6 @@ function Navbar() {
     <nav className="fixed top-0 absolute w-full h-[4rem] py-4 " >
       <ul className="flex items-center justify-center space-x-10 w-full px-6">
         <li className="text-[#98EC65] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link to="/card">Shop Now</Link></li>
-        <li className="text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/about">About</Link></li>
-        <li className="text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/contact">Contact</Link></li>
-        <li className="text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/">Log In</Link></li>
-        <li className="text-[#fff] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/cart"><ShoppingCart size={32} /></Link></li>
         <li className="text-[#000] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/about">About</Link></li>
         <li className="text-[#000] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/contact">Contact</Link></li>
         <li className="text-[#000] font-bold text-[1.1rem] cursor-pointer hover:animate-pulse "><Link className='a' to="/">Log In</Link></li>
