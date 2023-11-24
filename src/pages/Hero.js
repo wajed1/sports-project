ff8681c68f0c4d746e74b0d671b4a7da8833e86f
wajed1 <wajed.basbous@net.usj.edu.lb>
2023-11-24 10:03:46 +0200

    hero text color

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -23,7 +23,7 @@ function Hero() {
 
     <div className='h-full w-full flex flex-col justify-center px-[4rem] text-left' >
           <h1 className='text-[#98EC65] font-bold text-[5rem]' >VShop</h1>
-          <h3 className='text-white text-[2rem] font-bold' >Freshness delivered to your door – explore a garden of vibrant, farm-fresh vegetables for a healthier, tastier lifestyle.</h3>
+          <h3 className='text-grey text-[2rem] font-bold' >Freshness delivered to your door – explore a garden of vibrant, farm-fresh vegetables for a healthier, tastier lifestyle.</h3>
 
           <button className='w-[10rem] py-[6px] rounded-3xl bg-[#98EC65] hover:bg-[#81E047] mt-[1rem]' onClick={handleClick} >Explore Now</button>
       </div>
